fad4bc546a1c672ecc1fd727b52d9370274804b9
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-03-18 20:08:02 +0700

    Membikin Menu

---
+++ File3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int Pilihan;
+    
+    cout << "===Menu===" << endl;
+    cout << "1. Menghitung Luas Persegi" << endl;
+    cout << "2. Menghitung Luas Lingkaran" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Pilih (1/2/3) : ";
+    cin >> Pilihan;
+}
