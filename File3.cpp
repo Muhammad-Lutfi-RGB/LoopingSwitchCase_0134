c1f976930bfd506d09632dfa3a33401c140f8e18
Muhammad-Lutfi-RGB <muhammad.lutfi.ft24@mail.umy.ac.id>
2025-03-18 20:15:41 +0700

    Menambahkan Switch dan Case untuk masing - masing pilihan

--- File3.cpp
+++ File3.cpp
@@ -10,4 +10,19 @@ int main() {
     cout << "3. Exit" << endl;
     cout << "Pilih (1/2/3) : ";
     cin >> Pilihan;
+
+    switch (Pilihan)
+    {
+    case 1 :
+        cout << "Menghitung Luas Persegi Panjang" << endl;
+        break;
+    case 2 :
+        cout << "Menghitung Luas Lingkaran" << endl;
+    case 3 :
+        break;
+
+    default:
+        cout << "Pilihan Anda Salah" << endl;
+        break;
+    }
 }
